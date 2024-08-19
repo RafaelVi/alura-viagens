313d4e029839bbaaeb8a32d772655cd89501c633
Rafael Vitor de Almeida <43658933+RafaelVi@users.noreply.github.com>
2024-08-19 15:04:44 -0300

    update: use AppCompatResources

--- app/src/main/java/br/com/rafaelvi/aluraviagens/util/ResourceUtil.java
+++ app/src/main/java/br/com/rafaelvi/aluraviagens/util/ResourceUtil.java
@@ -1,16 +1,13 @@
 package br.com.rafaelvi.aluraviagens.util;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 
-import br.com.rafaelvi.aluraviagens.model.Pacote;
+import androidx.appcompat.content.res.AppCompatResources;
 
 public class ResourceUtil {
     public static Drawable devolveDrawable(String drawable, Context context) {
-        Resources resource = context.getResources();
-        int idDoDrawable = resource.getIdentifier(drawable, "drawable", context.getPackageName());
-        Drawable drawableImagemPacote = resource.getDrawable(idDoDrawable);
-        return drawableImagemPacote;
+        int id = context.getResources().getIdentifier(drawable, "drawable", context.getPackageName());
+        return AppCompatResources.getDrawable(context, id);
     }
 }
